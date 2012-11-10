773a7957faeea215ea26e70a861da970e7b0d472
Ellipsis002 <ellipsis002@Ellipsis002s-Mac-mini.local>
2012-11-10 10:15:49 +0530

    Initial Commit

---
+++ FirstProjectNew/FPAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  FPAppDelegate.h
+//  FirstProjectNew
+//
+//  Created by Ellipsis002 on 11/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class FPViewController;
+
+@interface FPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) FPViewController *viewController;
+
+@end
